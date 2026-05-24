05ba00b87d0dde7aa4004d225ec31f9438de6166
SNEHASREE JATI <snehasreejati5@gmail.com>
2026-05-24 16:19:59 +0530

    Implement session persistence in Firebase auth
    Added session persistence for authentication.

--- firebase.js
+++ firebase.js
@@ -1,6 +1,10 @@
 import { initializeApp } from "firebase/app";
 
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDj3zk2BlAqxmt3Ad-QosdwRqYsyXYSuFo",
@@ -14,3 +18,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+setPersistence(auth, browserSessionPersistence);
