770396a939fde47e8a539f5f2ae7fa3d83c7ebcf
Kamalika Bhattacharya <kamalikabhattacharya554@gmail.com>
2026-05-24 22:16:25 +0530

    Refactor main.jsx to simplify rendering

--- main.jsx
+++ main.jsx
@@ -1,42 +1,10 @@
-import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-import ReactDOM from "react-dom/client";
-
-import {
-  BrowserRouter,
-  Routes,
- Route,
-} from "react-router-dom";
-
-import Login from "./pages/Login";
-
-import App from "./App";
-
-import "./index.css";
-
-ReactDOM.createRoot(
-  document.getElementById("root")
-).render(
-
-  <React.StrictMode>
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/dashboard"
-          element={<App />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
